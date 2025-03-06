ef8e422699a1d918f6e196f0b35b887679c876a6
Douggie 🐱 <okrzesikbankston@gmail.com>
2025-03-06 12:53:10 -0600

    Create config.lua

---
+++ config.lua
@@ -0,0 +1,7 @@
+-- Configurable departments with ACE permissions, blip colors, and weapon loadouts
+departments = {
+    { name = 'Police', permission = 'duty.police', blipColor = 3, weapons = { 'WEAPON_PISTOL', 'WEAPON_STUNGUN', 'WEAPON_CARBINERIFLE' } },
+    { name = 'Fire', permission = 'duty.fire', blipColor = 1, weapons = { 'WEAPON_FIREEXTINGUISHER', 'WEAPON_FLASHLIGHT' } },
+    { name = 'EMS', permission = 'duty.ems', blipColor = 2, weapons = { 'WEAPON_FLASHLIGHT', 'WEAPON_STUNGUN' } },
+    { name = 'Dispatch', permission = 'duty.dispatch', blipColor = 5, weapons = {} }
+}
