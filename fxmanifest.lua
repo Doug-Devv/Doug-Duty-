d95d00b4ce50f42f912e2563ff1042d13226922c
Douggie 🐱 <okrzesikbankston@gmail.com>
2025-03-06 12:52:32 -0600

    Create fxmanifest.lua

---
+++ fxmanifest.lua
@@ -0,0 +1,23 @@
+fx_version 'cerulean'
+game 'gta5'
+
+author 'Your Name'
+description 'Freech Duty - Advanced Duty Management for FiveM'
+version '1.0.0'
+
+server_scripts {
+    'server/server.lua'
+}
+
+client_scripts {
+    'client/client.lua'
+}
+
+shared_scripts {
+    'config/config.lua'
+}
+
+files {
+    'plugins/*.lua'
+}
+
